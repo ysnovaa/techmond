2e53dff3fce59df641fe52bedfe15ff13cc5e494
Yhilmar <yachaverrc@eafit.edu.co>
2023-11-04 13:59:23 -0500

    pruebas unitarias y apirest

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -38,7 +38,6 @@ urlpatterns = [
     path('delete-order/<int:pk>', views.delete_order_view,name='delete-order'),
     path('update-order/<int:pk>', views.update_order_view,name='update-order'),
 
-
     path('customersignup', views.customer_signup_view),
     path('customerlogin', LoginView.as_view(template_name='ecom/customerlogin.html'),name='customerlogin'),
     path('customer-home', views.customer_home_view,name='customer-home'),
