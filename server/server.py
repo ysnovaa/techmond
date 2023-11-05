9b7472c3d80934cd779a9dd8bd546f01ee2bdff1
Yhilmar <yachaverrc@eafit.edu.co>
2023-11-05 09:14:49 -0500

    dockerfile y dockercompose

--- server/server.py
+++ server/server.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/get_products', methods=['GET'])
 def get_products():
-    django_url = 'http://localhost:8000/api/products/'  
+    django_url = 'http://127.0.0.1:8000/api/products/'  
     response = requests.get(django_url)
     products = response.json()
     return jsonify(products)
 
 @app.route('/get_product/<int:product_id>', methods=['GET'])
 def get_product(product_id):
-    django_url = f'http://localhost:8000/api/products/{product_id}/'  
+    django_url = f'http://127.0.0.1:8000/api/products/{product_id}/'  
     response = requests.get(django_url)
     if response.status_code == 200:
         product = response.json()
@@ -21,4 +21,4 @@ def get_product(product_id):
         return f'Error al obtener el producto con ID {product_id}'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
